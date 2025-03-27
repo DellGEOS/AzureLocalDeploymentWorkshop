1aef41d059b5af59819ac4168184ec35bcdbe730
Matt McSpirit <mattmc@outlook.com>
2025-03-27 11:49:05 -0700

    WinSvr Image Fixes

--- artifacts/labconfig/AzureLocalLabConfig.ps1
+++ artifacts/labconfig/AzureLocalLabConfig.ps1
@@ -18,4 +18,4 @@ $LabConfig = @{ DomainAdminName = '<<DomainAdminName>>'; AdminPassword = '<<Admi
 <<installWAC>>
 
 #Management machine
-#$LabConfig.VMs += @{ VMName = 'MGMT' ; ParentVHD = 'Win2022_G2.vhdx'; MGMTNICs=1 ; AddToolsVHD=$True }
+#$LabConfig.VMs += @{ VMName = 'MGMT' ; ParentVHD = 'WinSvr_G2.vhdx'; MGMTNICs=1 ; AddToolsVHD=$True }
